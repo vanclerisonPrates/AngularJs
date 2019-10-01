ab3f28cae73f93c60c3998ac989be3452f4ed283
vanclerison <vans.prates@outlook.com>
2019-09-30 23:11:49 -0300

    adicionando a promise

--- public/js/controllers/foto-controller.js
+++ public/js/controllers/foto-controller.js
@@ -1,7 +1,7 @@
 // public/js/controllers/foto-controller.js
 
 angular.module('alurapic')
-    .controller('FotoController', function($scope, recursoFoto, $routeParams) {
+    .controller('FotoController', function($scope, recursoFoto, $routeParams, cadastroDeFotos) {
 
        
 
@@ -20,29 +20,14 @@ angular.module('alurapic')
         $scope.submeter = function() {
 
             if ($scope.formulario.$valid) {
-
-                if($routeParams.fotoId) {
-
-                   // Novidade aqui! Usando nosso update!
-
-                    recursoFoto.update({fotoId: $scope.foto._id}, 
-                        $scope.foto, function() {
-                        $scope.mensagem = 'Foto alterada com sucesso';
-                    }, function(erro) {
-                        console.log(erro);
-                        $scope.mensagem = 'Não foi possível alterar';
-                    });
-
-                } else {
-                    recursoFoto.save($scope.foto, function() {
-                        $scope.foto = {};
-                        $scope.mensagem = 'Foto cadastrada com sucesso';
-                    }, function(erro) {
-                        console.log(erro);
-                        $scope.mensagem = 'Não foi possível cadastrar a foto';
-                    });
-                }
+                cadastroDeFotos.cadastrar($scope.foto)
+                .then(function(dados){
+                    $scope.mensagem = dados.mensagem;
+                    if(dados.inclusao) $scope.foto = {};
+                })
+                .catch(function(erro){
+                    $scope.mensagem = erro.mensagem;
+                });
             }
         };
-
     });
--- public/js/services/meus-servicos.js
+++ public/js/services/meus-servicos.js
@@ -4,6 +4,43 @@ angular.module('meusServicos', ['ngResource'])
         return $resource('/v1/fotos/:fotoId', null, { 
             'update':{
             method: 'PUT'
-        }
+            }
+        })
+    })
+    .factory('cadastroDeFotos', function(recursoFoto, $q){
+        var service = {};
+        service.cadastrar = function(foto){
+            return $q(function(resolve,reject){
+                if(foto._id){
+                    recursoFoto.update({fotoId: foto._id}, foto, function(){
+                        resolve({
+                            mensagem: 'Foto' + foto.titulo + 'autalizada com sucesso',
+                            inclusao: false
+                        });
+                    
+                }, function(erro){
+                    console.log(erro);
+                    reject({
+                        mensagem: 'Nao foi possivel atualizar a foto ' + foto.titulo
+                    });
+                });
+                } else {
+                    recurso.save(foto, function(){
+                        resolve({
+                            mensagem: 'Foto ' + foto.titulo + 'incluida com sucesso',
+                            inclusao: true
+                        });
+                    
+                }, function(erro){
+                        console.log(erro);
+                        reject({
+                            mensagem: 'Nao foi possivel incluir a foto ' + foto.titulo
+                        });
+                });
+
+                }
+        });
+    };
+            return service;
     });
-});
+
